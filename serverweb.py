986658f99be4e4fee6eaf64428a72bacec5c6149
vitorezende <vitorezende@gmail.com>
2017-10-08 22:01:35 -0300

    Adicionado Comentários
    Comentando o código

--- serverweb.py
+++ serverweb.py
@@ -1,17 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Universidade Federal de Mato Grosso - Campus Universitario do Araguaia
+Dicente: Vitor Rezende Campos
+Docente: Maxwell Carmo
+Disciplina: Redes de Computadores
+
+Servidor Web Simples - Python 2.7
+    
+"""
 import socket
- 
-HOST = "127.0.0.1"
-PORTA = 8181
- 
+
+HOST = "127.0.0.1" 	# IP DO SERVIDOR
+PORTA = 8181		# Porta do Servidor
+
+# Criando Socket e Vinculando com a Porta que será ouvida as requisições do Navegador(Cliente)
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 servidor.bind((HOST, PORTA))
 servidor.listen(6)
 
 
- 
+# Função para obter nome da página requisitada 
 def extrai_nome_doc(req):
 	return req.split()[1][1:]				#Quebro a requisicao escolho a parte que contem o nome do arquivo e tiro o primeiro elemento que eh o barra
-	
+
+# Função que constrói resposta HTTP para o navegador (cliente)	
 def constroi_resposta(doc):
 	res = "HTTP/1.1 200 OK\r\n\r\n"
 	if doc == "":
